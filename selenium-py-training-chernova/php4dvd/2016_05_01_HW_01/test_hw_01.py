43f5ef598c4791eb789782ade28a72a6fb3fe6e5
anache <ana.anache@gmail.com>
2016-05-08 19:22:50 -0700

    create and delete movie tests

--- selenium-py-training-chernova/php4dvd/2016_05_01_HW_01/test_hw_01.py
+++ selenium-py-training-chernova/php4dvd/2016_05_01_HW_01/test_hw_01.py
@@ -7,15 +7,15 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-class Untitled(unittest.TestCase):
+class HomeWork01(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
         self.base_url = "http://localhost/"
         self.verificationErrors = []
         self.accept_next_alert = True
-    
-    def test_untitled(self):
+
+    def test_home_work01(self):
         driver = self.driver
         driver.get(self.base_url + "/php4dvd/")
         driver.find_element_by_id("username").clear()
@@ -23,17 +23,19 @@ class Untitled(unittest.TestCase):
         driver.find_element_by_name("password").clear()
         driver.find_element_by_name("password").send_keys("admin")
         driver.find_element_by_name("submit").click()
-    
+        driver.find_element_by_link_text("Log out").click()
+        self.assertRegexpMatches(self.close_alert_and_get_its_text(), r"^Are you sure you want to log out[\s\S]$")
+
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
-    
+
     def is_alert_present(self):
         try: self.driver.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
-    
+
     def close_alert_and_get_its_text(self):
         try:
             alert = self.driver.switch_to_alert()
@@ -44,11 +46,10 @@ class Untitled(unittest.TestCase):
                 alert.dismiss()
             return alert_text
         finally: self.accept_next_alert = True
-    
+
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
-
